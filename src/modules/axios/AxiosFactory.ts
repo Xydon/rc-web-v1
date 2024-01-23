c2a2eeb3de61aecdb931076402750b7af4b39050
jayantpranjal0 <96689162+jayantpranjal0@users.noreply.github.com>
2024-01-24 00:52:02 +0530

    Update AxiosFactory.ts

--- src/modules/axios/AxiosFactory.ts
+++ src/modules/axios/AxiosFactory.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, CreateAxiosDefaults } from "axios";
 
 export default class AxiosFactory {
 	static createInstance<T = any>(config?: CreateAxiosDefaults<T>) {
-		const basePath = "http://ec2-13-53-132-182.eu-north-1.compute.amazonaws.com/";
+		const basePath = "https://rc-iitbhu.com/";
 		// const basePath = "https://www.lohawalla.com/";
 		const _config = config ? config : ({} as CreateAxiosDefaults<any>);
 		const burl = _config.baseURL ? _config.baseURL : "";
